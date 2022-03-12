602c4d4d4297c21c639c2cd6115403011d1ad9c7
jyyoun1022 <jyyoun1022@naver.com>
2022-03-12 17:56:57 +0900

    회원 삭제 처리(@Transactional and @Commit)

--- src/main/java/org/zerock/mreview/reposiotry/ReviewRepository.java
+++ src/main/java/org/zerock/mreview/reposiotry/ReviewRepository.java
@@ -2,6 +2,7 @@ package org.zerock.mreview.reposiotry;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.zerock.mreview.entity.Member;
 import org.zerock.mreview.entity.Movie;
@@ -14,6 +15,7 @@ public interface ReviewRepository extends JpaRepository<Review,Long> {
     @EntityGraph(attributePaths = {"member"}, type=EntityGraph.EntityGraphType.FETCH)
     List<Review> findByMovie (Movie movie);
 
+    @Modifying
     @Query("delete from Review mr where mr.member =:member")
     void deleteByMember(Member member);
 
